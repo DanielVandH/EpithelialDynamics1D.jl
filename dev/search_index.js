10c3e830f0492d4d633aa22a40c372a67b3a2dfa
Documenter.jl <documenter@juliadocs.github.io>
2023-06-06 08:06:34 +0000

    build based on 271901f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EpithelialDynamics1D","category":"page"},{"location":"#EpithelialDynamics1D","page":"Home","title":"EpithelialDynamics1D","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EpithelialDynamics1D.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EpithelialDynamics1D]","category":"page"}]
+}
